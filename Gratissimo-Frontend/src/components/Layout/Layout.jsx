cd663ffa8756f9b00dc548b7812dad4c7ca33628
MathiasNielsen99 <math324e@elev.techcollege.dk>
2026-09-17 18:42:07 +0200

    First made the login reminder component used authContext to see if we are loged in in order to remove it once loged in otherwise its just in layout,

--- Gratissimo-Frontend/src/components/Layout/Layout.jsx
+++ Gratissimo-Frontend/src/components/Layout/Layout.jsx
@@ -1,12 +1,16 @@
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
+import { LoginReminder } from "../LoginReminder/LoginReminder";
 import "./Layout.module.scss";
 
 export function Layout({ children }) {
   return (
     <div className="layout">
       <Header />
-      <main className="layout-content">{children}</main>
+      <main className="layout-content">
+        <LoginReminder />
+        {children}
+      </main>
       <Footer />
     </div>
   );
